1b06e1f96d0c4db0f9febc4ae3aab57b961157ec
smae1993 <smae1993@gmail.com>
2025-05-20 09:40:41 +0330

    working on lesson 3 project

---
+++ Lesson_3/project_3/src/Funbyte.js
@@ -0,0 +1,12 @@
+
+
+
+export default function Funbyte(props) {
+    console.log(props);
+    return (
+        <div>
+            <h1>Funbyte</h1>
+            <p>{props.description}</p>
+        </div>
+    );
+}
