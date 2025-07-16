bea739f5df6758bc34990259dc89c976fb090a5c
smae1993 <smae1993@gmail.com>
2025-07-16 13:46:20 +0330

    working on lesson 10

--- src/pages/FilmsPageWithMui.js
+++ src/pages/FilmsPageWithMui.js
@@ -3,14 +3,9 @@ import React, { useState, useEffect } from 'react';
 import {
     Card,
     CardContent,
-    CardMedia,
-    Typography,
-    Button,
-    Box,
+
     Skeleton,
-    Badge,
     useTheme,
-    styled,
 } from '@mui/material';
 import FilmCard from "../components/FilmCard";
 
@@ -35,11 +30,6 @@ export default function FilmsPageWithMui() {
         fetchMovies();
     }, []);
 
-    const getImdbRating = (imdbID) => {
-        // This is a placeholder function. In a real app, you would fetch the full movie details
-        // to get the IMDb rating. For now, we'll just return a random rating between 6 and 10.
-        return Math.floor(Math.random() * 5 + 6);
-    };
 
     return (
         <div className="grid grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-4 p-4">
